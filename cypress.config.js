eb63ba40901631af9ff91834bf5dc0464c8b0203
Lucas Beninca <lebbeninca@gmail.com>
2024-11-08 16:20:56 -0300

    up

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,23 @@
 const { defineConfig } = require('cypress')
 
 const puppeteerSetup = require('./cypress/support/puppeteer')
-const { getChromiumWebBrowsers } = require('./cypress/support/utils')
+//const { getChromiumWebBrowsers } = require('./cypress/support/utils')
 
 module.exports = defineConfig({
   projectId: 'u8w8pa',
   e2e: {
     setupNodeEvents(on, config) {
       puppeteerSetup(on)
+      setup({
+        on,
+        onMessage: {
+          async myMessageHander (browser) {
+            // Utilize the Puppeteer browser instance and the Puppeteer API to interact with and automate the browser
+          },
+        },
+      })
 
-      return getChromiumWebBrowsers(config)
+      // return getChromiumWebBrowsers(config)
     },
   },
   fixturesFolder: false,
